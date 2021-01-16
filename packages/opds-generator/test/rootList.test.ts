fe75ba8fd1c5a2a27e21d1a08e05331696a2f525
Aki Rautio <aki@akirautio.com>
2021-01-16 13:05:05 +0200

    Change .np-config and fix tests

--- packages/opds-generator/test/rootList.test.ts
+++ packages/opds-generator/test/rootList.test.ts
@@ -6,14 +6,38 @@ describe('rootList', () => {
     it('should form valid xml', () => {
       expect(
         xmlParser.validate(
-          rootList.root({ baseDir: '/', updated: new Date('2020-01-01') })
+          rootList.root({
+            baseDir: '/',
+            updated: new Date('2020-01-01'),
+            options: [
+              {
+                title: 'Popular',
+                type: 'entry',
+                link: '/books',
+                updated: new Date('2020-01-02'),
+                description: 'Popular books',
+              },
+            ],
+          })
         )
       ).toEqual(true);
     });
 
     it('should generate base item', () => {
       expect(
-        rootList.root({ baseDir: '/', updated: new Date('2020-01-01') })
+        rootList.root({
+          baseDir: '/',
+          updated: new Date('2020-01-01'),
+          options: [
+            {
+              title: 'Popular',
+              type: 'entry',
+              link: '/books',
+              updated: new Date('2020-01-02'),
+              description: 'Popular books',
+            },
+          ],
+        })
       ).toMatchSnapshot();
     });
   });
