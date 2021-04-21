b893395545ed82a293fec1cfede506847392f027
Aki Rautio <aki@akirautio.com>
2021-04-21 08:28:24 +0300

    Component changes * Make button work * Move menubar as a component * Change to ResponsiveMenu and Button

--- packages/web/src/components/Button.svelte
+++ packages/web/src/components/Button.svelte
@@ -1,6 +1,22 @@
+<script lang="ts">
+  export let ref: null | HTMLButtonElement = null;
+  export let type = "button";
+  export let disabled = false;
+</script>
+
 <button
-  type="button"
-  class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+  class="
+    py-2 px-4
+   bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white 
+   w-full transition ease-in duration-200 
+   text-center text-base font-semibold shadow-md 
+   focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg
+   "
+  {...$$restProps}
+  {type}
+  {disabled}
+  bind:this={ref}
+  on:click
 >
   <slot />
 </button>
